c8c9161eaa4de450e92e8cd852445f0ce3875eb6
ntospa21 <tommyaek@gmail.com>
2021-10-27 21:11:52 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+document.title ='Weather App'
 ReactDOM.render(
   <React.StrictMode>
     <App />
